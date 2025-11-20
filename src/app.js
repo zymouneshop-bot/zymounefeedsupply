0cb971aab27600540bc9de973e9fa76f44e578fc
Trina <zymouneshop@gmail.com>
2025-11-20 23:02:22 +0800

    Add cache-control to CORS allowed headers

--- src/app.js
+++ src/app.js
@@ -164,7 +164,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'cache-control']
 }));
 
 app.use(express.json({ limit: '50mb' }));
